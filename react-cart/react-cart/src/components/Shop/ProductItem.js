7b53efb71bed2994233436797527af7b003766c9
dhruvi6071 <dhruvi6071@gmail.com>
2024-06-02 23:32:11 +0530

    add-dummy-data-hard-core

--- react-cart/react-cart/src/components/Shop/ProductItem.js
+++ react-cart/react-cart/src/components/Shop/ProductItem.js
@@ -1,5 +1,39 @@
-import Card from '../UI/Card';
-import classes from './ProductItem.module.css';
+import Card from "../UI/Card";
+import classes from "./ProductItem.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 6,
+    title: "My first Item",
+    description: "This is my first Item.",
+  },
+  {
+    id: "p2",
+    price: 6,
+    title: "My second Item",
+    description: "This is my second Item.",
+  },
+  {
+    id: "p3",
+    price: 6,
+    title: "My third Item",
+    description: "This is my third Item.",
+  },
+  {
+    id: "p4",
+    price: 6,
+    title: "My fourth Item",
+    description: "This is my fourth Item.",
+  },
+  {
+    id: "p5",
+    price: 6,
+    title: "My fifth Item",
+    description: "This is my fifth Item.",
+  },
+ 
+];
 
 const ProductItem = (props) => {
   const { title, price, description } = props;
--- react-cart/react-cart/src/components/Shop/Products.js
+++ react-cart/react-cart/src/components/Shop/Products.js
@@ -1,16 +1,53 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 6,
+    title: "My first Item",
+    description: "This is my first Item.",
+  },
+  {
+    id: "p2",
+    price: 6,
+    title: "My second Item",
+    description: "This is my second Item.",
+  },
+  {
+    id: "p3",
+    price: 6,
+    title: "My third Item",
+    description: "This is my third Item.",
+  },
+  {
+    id: "p4",
+    price: 6,
+    title: "My fourth Item",
+    description: "This is my fourth Item.",
+  },
+  {
+    id: "p5",
+    price: 6,
+    title: "My fifth Item",
+    description: "This is my fifth Item.",
+  },
+ 
+];
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+          key = {product.id}
+          title={product.title}
+          price={product.price}
+          description={product.description}
         />
+       ))}
+        
       </ul>
     </section>
   );
