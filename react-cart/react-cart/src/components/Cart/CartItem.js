8ff69104d62aa09521b3f72c0db67fb2b57ef689
dhruvi6071 <dhruvi6071@gmail.com>
2024-06-03 22:57:47 +0530

    + and - button-working

--- react-cart/react-cart/src/components/Cart/CartItem.js
+++ react-cart/react-cart/src/components/Cart/CartItem.js
@@ -1,7 +1,22 @@
+import { useDispatch } from 'react-redux';
+import { cartAction } from '../../store/cart-slice';
 import classes from './CartItem.module.css';
 
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+  const dispatch =  useDispatch();
+  const { title, quantity, total, price, id } = props.item;
+
+  const removeItemHandler = () => {
+    dispatch(cartAction.removeItemFromCart(id));
+  };
+
+  const addItemHandler = () => {
+    dispatch(cartAction.addItemToCart({
+      id: id,
+      title: title,
+      price: price
+    }));
+  };
 
   return (
     <li className={classes.item}>
@@ -17,8 +32,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={removeItemHandler}>-</button>
+          <button onClick={addItemHandler}>+</button>
         </div>
       </div>
     </li>
