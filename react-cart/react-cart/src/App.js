d555141d2f924206e7040190a10fd039c6134c21
dhruvi6071 <dhruvi6071@gmail.com>
2024-06-14 16:05:01 +0530

    using-action-creator-'thunk'-

--- react-cart/react-cart/src/App.js
+++ react-cart/react-cart/src/App.js
@@ -4,8 +4,8 @@ import { Fragment, useEffect } from "react";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { uiActions } from "./store/ui-slice";
 import Notification from "./components/UI/Notification";
+import { sendCartData } from "./store/cart-slice";
 
 let isInitial = true;
 function App() {
@@ -16,49 +16,12 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'pending',
-          title: 'sending...',
-          message: 'sending cart data',
-        })
-      );
-      //By this put method we can set the new incoming data.
-      const response = await fetch(
-        "https://cart-457ba-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
-        //Here put method override the existing data.
-        { method: "PUT", body: JSON.stringify(cart) }
-      );
-
-      if (!response.ok) {
-        throw new Error("Sending cart data failed");
-      }
-
-      
-      dispatch(
-        uiActions.showNotification({
-          status: 'success',
-          title: 'Success',
-          message: "sent cart data successfully",
-        })
-      );
-    };
-
     if (isInitial) {
       isInitial = false;
       return;
     }
 
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'success',
-          title: 'Success',
-          message: "sent cart data successfully",
-        })
-      );
-    });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
   return (
@@ -68,6 +31,8 @@ function App() {
         {showCart && <Cart />}
         <Products />
       </Layout>
+      {showCart && <Cart />}
+      <Products />                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
     </Fragment>
   );
 }
--- react-cart/react-cart/src/store/cart-slice.js
+++ react-cart/react-cart/src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./ui-slice";
 
 const cartSlice = createSlice({
   name: "cart",
@@ -7,7 +8,12 @@ const cartSlice = createSlice({
     totalQuantity: 0,
     totalAmount: 0,
   },
+
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
@@ -38,6 +44,51 @@ const cartSlice = createSlice({
     },
   },
 });
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "sending...",
+        message: "sending cart data",
+      })
+    );
+
+    const sendRequest = async () => {
+      //By this put method we can set the new incoming data.
+      const response = await fetch(
+        "https://cart-457ba-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
+        //Here put method override the existing data.
+        { method: "PUT", body: JSON.stringify(cart) }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed");
+      }
+    };
+
+    await sendRequest();
+
+    try {
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success",
+          message: "sent cart data successfully",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "error",
+          message: "sent cart data failed",
+        })
+      );
+    }
+  };
+};
 
 export const cartAction = cartSlice.actions;
 
